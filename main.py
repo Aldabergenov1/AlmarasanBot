a98fe3ebbfcdc351a2c6ea340126672c55baf0f0
Aldabergenov1 <elliotaldersson333@gmail.com>
2024-02-09 21:58:53 +0600

    first commit

---
+++ main.py
@@ -0,0 +1,17 @@
+from aiogram import Bot, Dispatcher, F
+from aiogram.types import Message
+from aiogram.filters import CommandStart, Command, StateFilter
+from config import TOKEN
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import StatesGroup, default_state, State
+
+bot = Bot(TOKEN)
+dp = Dispatcher()
+
+
+@dp.message(CommandStart(), StateFilter(default_state))
+async def start(message: Message):
+    await message.answer("")
+
+
+@dp.message()
